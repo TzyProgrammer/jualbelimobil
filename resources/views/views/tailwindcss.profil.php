9e5351ddc5fca3f8ade5fc21ac078a78d1728aed
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-04-25 15:20:33 +0700

    Perbaiki folder Controllers dan Views, Install tailwind

--- resources/views/views/tailwindcss.profil.php
+++
@@ -1,4 +0,0 @@
-<link rel=”stylesheet” href=”https://cdn.tailwindcss.com/3.4.1”>
-<link href="https://cdn.tailwindcss/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-<link rel=”stylesheet” href=”/style/tailwindcss3.4.1.js”>
-
