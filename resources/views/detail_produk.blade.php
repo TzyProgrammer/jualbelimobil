b88f531d8523c8506f9ff0cf5ea654c64804d57f
Arwindo <arwindoid@gmail.com>
2024-04-27 17:57:53 +0700

    pengerjaan layout detail produk dan favorite

---
+++ resources/views/detail_produk.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
+    <title>Document</title>
+</head>
+<body>
+<div class="p-6 bg-white">
+  <h1 class="text-4xl mb-16 mt-4">Nama Mobil</h1>
+   <div class="bg-white p-2">
+    <h1 class="text-3xl mb-2">Judul Besar</h1>
+     <p class="text-2xl text-black">Deskripsi</p>
+     <p class="text-2xl text-black">Deskripsi</p>
+     <p class="text-2xl text-black">Deskripsi</p>
+     <p class="text-2xl text-black">Deskripsi</p>
+</div>
+<div class="text-3xl text-black mb-2 mt-2">Rp 1.000.000.000</div>
+<div>
+   <div class="bg-white p-2">
+    <h1 class="mb-2">Note</h1>
+     <p class="text-black">Deskripsi</p>
+     <p class="text-black">Deskripsi</p>
+     <p class="text-black">Deskripsi</p>
+     <p class="text-black">Deskripsi</p>
+</div>
+   <div class="flex space-x-4 mb-6 text-sm font-medium">
+      <div class="flex-auto flex space-x-44">
+   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 rounded">
+    Beli
+  </button>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</body>
+</html>
---
+++ resources/views/favorite.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
+    <title>Document</title>
+</head>
+<body>
+<div class="container mx-auto px-2 pt-6">
+    <ul class="w-full text-center text-sm">
+        <li class="text-4xl border-b pt-8 py-12 font-bold">FAVORIT</li>
+        <li class="text-2xl border-b py-3">NAMA MOBIL 1 <br> Rp 1.000.000.000</br></li>
+        <li class="text-2xl border-b py-3">NAMA MOBIL 2 <br> Rp 1.000.000.000</br></li>
+        <li class="text-2xl border-b py-3">NAMA MOBIL 3 <br> Rp 1.000.000.000</br></li>
+    </ul>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</body>
+</html>
