38c4c08de7f9f750742d4afcd271b175d7c183c5
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-05-25 22:41:12 +0700

    Update components navbar & Penambahan components footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="p-3 bg-gray-200 columns-2">
+    <div class="">
+        <img src="images/logokecil.png" alt="Logo">
+        <p class="pr-8">is a web-based application that sells various types of car brands. This application was created to make it easier for buyers so that buyers don't have to visit the place directly to see the car, just go through the website to see the details of the car and also with more flexible times.</p>
+        <div class="flex py-10 items-center">
+            <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z"/>
+              </svg>
+              <span class="text-lg pl-2">097-8654-3296</span>          
+        </div>
+    </div>
+    <div class="break-before-column pl-40 pt-4">
+        <div class="flex items-center">
+            <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="location"><path d="M12,2a8.009,8.009,0,0,0-8,8c0,3.255,2.363,5.958,4.866,8.819,0.792,0.906,1.612,1.843,2.342,2.791a1,1,0,0,0,1.584,0c0.73-.948,1.55-1.885,2.342-2.791C17.637,15.958,20,13.255,20,10A8.009,8.009,0,0,0,12,2Zm0,11a3,3,0,1,1,3-3A3,3,0,0,1,12,13Z"></path></svg>
+            <span class="text-lg pl-2">Find our location</span>
+        </div>
+        <p class="pl-2 pt-4 pr-16">Jl. Ahmad Yani, Tlk. Tering, Kec. Batam Kota, Kota Batam, Kepulauan Riau 29461</p>
+    </div>
+</footer>
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
     <title>@yield('title', 'Aplikasi Web Jual Beli Mobil')</title>
 </head>
@@ -11,15 +12,16 @@
     @include('components.navbar')
 
     <div class="pt-20 px-6">
-        <div class="flex justify-between items-center">
+        <div class="items-center">
             <a href="/home">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
                   </svg></a>
-              <a href="/home">Home</a>
         </div>
         @yield('content')
     </div>
+
+    @include('components.footer')
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </body>
