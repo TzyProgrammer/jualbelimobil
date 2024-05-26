4a790f88ec6b2f095fd79f9451add5c02f80071b
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-05-26 14:55:46 +0700

    Update Katalog

--- resources/views/katalog.blade.php
+++ resources/views/katalog.blade.php
@@ -5,20 +5,22 @@
 @section('content')
 
 <div class="my-4 flex justify-between space-x-4">
-    <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-3 w-full text-xl dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">BARU</button>
-    <button type="button" class="focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-3 w-full text-xl dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">SECOND</button>
+    
+    <form class="w-full">   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Cari Mobil" required />
+            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800">Cari</button>
+        </div>
+    </form>
+
 </div>
-<div class="flex justify-between py-2 px-8">
-    <a href=""><p class="text-lg">Hatchback</p></a>
-    <a href=""><p class="text-lg">MPV</p></a>
-    <a href=""><p class="text-lg">SUV</p></a>
-    <a href=""><p class="text-lg">Sedan</p></a>
-    <a href=""><p class="text-lg">Wagon</p></a>
-    <a href=""><p class="text-lg">Coupe</p></a>
-    <a href=""><p class="text-lg">Van</p></a>
-    <a href=""><p class="text-lg">Sport</p></a>
-</div>
-<div class="flex justify-center mt-4 mb-8 space-x-8">
+<div class="flex justify-center mt-2 mb-8 space-x-8">
     <a href=""><img src="images/logo_hyundai.png" alt="Logo Hyundai" class="w-16"></a>
     <a href=""><img src="images/logo_nissan2.png" alt="Logo Nissan" class="w-16"></a>
     <a href=""><img src="images/logo_honda.png" alt="Logo Honda" class="w-16"></a>
