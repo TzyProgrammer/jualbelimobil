c0dc6f4e0fd2033b7693fa26d4b368dcc958d5e6
sabrinadewi36@gmail <sabrinadewi36@gmail.com>
2024-05-26 15:25:49 +0700

    menghilangkan dark mode

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,18 +9,18 @@
   
   <section class="bg-red-900 dark:bg-red-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
       <img class="mx-auto my-auto" src="images/logo.png" alt="logo" width="200" >
 
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <form class="space-y-4 md:space-y-6" action="#">
               <div>
-                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                      <input type="username" name="username" id="username" class="bg-red-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                      <input type="username" name="username" id="username" class="bg-red-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="">
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-red-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-red-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="">
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
@@ -29,9 +29,9 @@
                       </div>
                     
                   </div>
-                  <button type="submit" class="w-full text-white bg-red-700 hover:bg-red-700 focus:ring-4 focus:outline-1 focus:ring-primary-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+                  <button type="submit" class="w-full text-white bg-red-700 hover:bg-red-700 focus:ring-4 focus:outline-1 focus:ring-primary-300 font-medium rounded-full text-sm px-5 py-2.5 text-center">Login</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      belum memiliki akun? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Daftar</a>
+                      Belum memiliki akun? <a href="/daftar" class="font-medium text-primary-600 hover:underline">Daftar</a>
                   </p>
               </form>
           </div>
