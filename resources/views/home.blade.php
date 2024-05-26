4d77c8fc592e41971afa3d75cd8955f1d2fce93b
sabrinadewi36@gmail <sabrinadewi36@gmail.com>
2024-05-26 17:41:01 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('layout.main')
+
+@section('title', 'Katalog')
+
+@section('content')
+
+
   <title>Home</title>
   <script src="https://cdn.tailwindcss.com"></script>
 
-<div id="newsletter-banner" tabindex="-1" class="fixed top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-    <img src="images/logokecil.png" alt="logo" width="150" >
-    <div class="flex items-center max-w-96 mx-auto">
-    <!-- Input search -->
-    <input class="bg-red-50 border border-gray-300 rounded-lg py-2 px-4 w-80 text-gray-700  focus:outline-none focus:shadow-outline" id="search" type="text" placeholder="Search">
 
-    <!-- Tombol search -->
-    <button class="bg-red-900 text-white rounded-lg p-2 hover:bg-red-600 focus:outline-none focus:shadow-outline">
-        <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-            <path d="M11 4a7 7 0 00-7 7c0 3.859 3.14 7 7 7s7-3.141 7-7a7 7 0 00-7-7z"></path>
-            <path d="M21 21l-4.35-4.35"></path>
-        </svg>
-    </button>
-</div>
-    <img class="w-8 h-8 ml-2 my-10" src="images/love.png" alt="icon">   
-    <img class="w-8 h-8 ml-2 my-10" src="images/akun.png" alt="icon">
-</div>
+    <img class="flex w-full" src="images/carhome.jpg" alt="page">
 
-    <img class="flex w-full" src="images/familycar.jpg" alt="page">
+    <div class="my-4 flex justify-between space-x-4">
+    
+    <form class="w-full mx-5" >   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-red-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Cari Mobil" required />
+            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800">Cari</button>
+        </div>
+    </form>
+
+</div>
+    
 
     <div class="flex flex-row items-center justify-center my-20 " >
-    <button type="button" class="w-60 text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 ">BARU</button>
-    <button type="button" class="w-60 text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 ">SECOND</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">Hatchback</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">MPV</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">SUV</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 lg-2.5 me-2 mb-2">Sedan</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">Wagon</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">Coupe</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">Van</button>
+    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-1 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2">Sport</button>
     </div>
 
-    <div class="flex flex-row items-center justify-center my-20 " >
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Hatchback</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">MPV</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">SUV</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sedan</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Wagon</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Coupe</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Van</button>
-    <button type="button" class="focus:outline-none text-black bg-white-0 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sport</button>
-    </div>
-
-    <div class="flex flex-row items-center justify-center my-20">
-        <div class="container px-16 flex justify-between">
+    <div class="flex flex-row items-center justify-center my-20 space-x-2">
+        <div class="container px-16 flex justify-between ">
         <a href=""><img src="images/logo_hyundai.png" alt="Logo Hyundai" class="w-16"></a>
         <a href=""><img src="images/logo_nissan.png" alt="Logo Nissan" class="w-16"></a>
         <a href=""><img src="images/logo_honda.png" alt="Logo Honda" class="w-16"></a>
@@ -51,14 +49,14 @@
         <a href=""><img src="images/logo_bmw.png" alt="Logo BMW" class="w-16"></a>
         <a href=""><img src="images/logo_chevrolet.png" alt="Logo Chevrolet" class="w-16"></a>
     
-    |   </div>
+    </div>
     </div>
     
 
-    <div class="flex flex-row items-center justify-center my-20 bg-red-900">
-            <div class="max-w-sm bg-white border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-row items-center justify-center my-20 bg-red-900 space-x-4">
+            <div class="max-w-sm bg-white border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4 mb-4">
                 <a href="#">
-                    <img class="rounded-t-lg" src="images/bmw.png" alt="" />
+                    <img class="rounded-t-lg" src="images/bmw-3.png" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="#">
@@ -71,40 +69,43 @@
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
                     </a>
-                </div>
-                
+                </div> 
+            </div> 
+            <div class="max-w-sm bg-white border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4 mb-4">
+                <a href="#">
+                    <img class="rounded-t-lg" src="images/bmw-3.png" alt="" />
+                </a>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">BMW 5 Series</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-200 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Rp. 1.987.897.900,00
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div> 
             </div>
-
+            <div class="max-w-sm bg-white border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4 mb-4">
+                <a href="#">
+                    <img class="rounded-t-lg" src="images/bmw-3.png" alt="" />
+                </a>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">BMW 5 Series</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-200 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Rp. 1.987.897.900,00
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div> 
+            </div>            
     </div>
-
-
-
-    <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="images/logokecil.png" width="70" alt="Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">CARSFIVE™</a>. All Rights Reserved.</span>
-    </div>
-</footer>
 
 
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,10 +10,10 @@
   <section class="bg-red-900 dark:bg-red-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
-      <img class="mx-auto my-auto mt-6" src="images/logo.png" alt="logo" width="200" >
+      <img class="mx-auto my-auto mt-6" src="images/logo.png" alt="logo" width="150" >
 
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <form class="space-y-4 md:space-y-2" action="#">
+              <form class="space-y-4 md:space-y-4" action="#">
               <div>
                       <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
                       <input type="username" name="username" id="username" class="bg-red-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="">
