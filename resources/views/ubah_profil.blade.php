0aae251df78c6ddc4e10e786f6664d2739693397
sitiiftmh <160402856+sitiiftmh@users.noreply.github.com>
2024-06-03 02:01:48 +0700

    Update ubah_profil.blade.php

--- resources/views/ubah_profil.blade.php
+++ resources/views/ubah_profil.blade.php
@@ -6,33 +6,15 @@
 
 <div class= "flex flex-row" >
 
-  <div class="w-64 my-10">
+  <div class="w-64 my-10 mx-44">
     <img src="images/profil.jpg" alt="">
-    <button class="mt-2 text-sm text-gray-500 hover:text-gray-700">Unggah Foto</button>
+    <button class="mt-2 text-sm text-gray-500 hover:text-gray-77 ">Unggah Foto</button>
     <input type="file" style="display: none;">
-
 </div>
-<script>
-    document.querySelector('button').addEventListener('click', function() {
-        document.querySelector('input[type="file"]').click();
-    });
 
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        const file = this.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                document.querySelector('img').src = e.target.result;
-            }
-            reader.readAsDataURL(file);
-        }
-    });
-</script>
-
-  
 
 <form action="/profil" method="POST">
-<form class="max-w-sm mx-24 mb-20 ">
+<form class="max-w-sm mx-24 mb-20 ml-20">
   <div class="mb-5">
     <label class="block mb-2 text-sm font-medium text-gray-900">Username</label>
     <input type="text" id="text" class="w-96 shadow-sm bg-red-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" placeholder="" value="Username" />
@@ -51,7 +33,7 @@
   </div>
 
   <div>
-    <a href="profil" button type="submit" class="mr-8 w-44 text-white bg-red-800 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center">Simpan</button></a>
+    <button type="submit" a href="/ubahprofil" class="mb-40 mr-8 w-44 text-white bg-red-800 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center">Simpan</button>
 
     <a href="/profil" class=" text-white bg-red-800 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-16 py-2.5 text-center">batal</a>
 
