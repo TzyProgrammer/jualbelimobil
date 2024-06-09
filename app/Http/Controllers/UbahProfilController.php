e8e4269810fd500b0de7b9ae15b6b026c27f481e
sitiiftmh <sfaa2348@gmail.com>
2024-06-09 19:05:49 +0700

    Ubah Profil Controller

--- app/Http/Controllers/UbahProfilController.php
+++ app/Http/Controllers/UbahProfilController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class UbahProfilController extends Controller
 {
-    //
+    public function show()
+    {
+        //update
+    $profil = ubah_profil::find();
+    $profil -> username ='username';
+    $profil -> nama = 'nama';
+    $profil -> alamat ='alamat';
+    $profil -> no_hp = 'no_hp';
+    $profil -> save();
+    }
 }
