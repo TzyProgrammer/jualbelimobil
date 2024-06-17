b698e7fafb691ddcdbe4c26ada71f285ae56e873
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-06-17 18:47:41 +0700

    Update code agar dapat kembali ke halaman sebelumnya untuk pembeli dan tidak muncul pada halaman home

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -12,12 +12,15 @@
     @include('components.navbar')
 
     <div class="pt-20 px-6">
+        @if (Request::path() !== 'home')
         <div class="items-center">
-            <a href="/home">
+            <a href="javascript:history.back()">
                 <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
-                  </svg></a>
+                </svg>
+            </a>
         </div>
+        @endif
         @yield('content')
     </div>
 
