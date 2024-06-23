d48e2c1f68f108d20a325bd57d2769e16ae75c59
TzyProgrammer <classroomtarich@gmail.com>
2024-06-23 16:46:04 +0700

    update sql

---
+++ sql/dbjualbelimobilnew.sql
@@ -0,0 +1,241 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Jun 2024 pada 11.44
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbjualbelimobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `merek_mobil`
+--
+
+CREATE TABLE `merek_mobil` (
+  `kode_merek` int(11) NOT NULL,
+  `merek` varchar(30) NOT NULL,
+  `gambar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `merek_mobil`
+--
+
+INSERT INTO `merek_mobil` (`kode_merek`, `merek`, `gambar`) VALUES
+(1, 'Toyota', '1719131452.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `kode_mobil` int(11) NOT NULL,
+  `kode_merek` int(11) NOT NULL,
+  `nama_mobil` varchar(50) DEFAULT NULL,
+  `harga_mobil` int(20) DEFAULT NULL,
+  `gambar_mobil` varchar(50) DEFAULT NULL,
+  `deskripsi_mobil` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mobil`
+--
+
+INSERT INTO `mobil` (`kode_mobil`, `kode_merek`, `nama_mobil`, `harga_mobil`, `gambar_mobil`, `deskripsi_mobil`) VALUES
+(1, 1, 'Avanza 2016', 150000000, '1718987016.jpeg', 'Mobil Keluarga');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil_favorit`
+--
+
+CREATE TABLE `mobil_favorit` (
+  `id_favorit` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL,
+  `kode_mobil` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id_pembeli` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_pembeli` varchar(50) NOT NULL,
+  `nomor_hp` bigint(20) NOT NULL,
+  `foto_profil` varchar(255) DEFAULT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pembeli`
+--
+
+INSERT INTO `pembeli` (`id_pembeli`, `username`, `password`, `nama_pembeli`, `nomor_hp`, `foto_profil`, `alamat`) VALUES
+(2, 'pembeli1', 'pembeli1', 'Pembeli 1', 81234567890, '', ''),
+(3, 'Test', 'rahasia', 'Tester', 81234567890, NULL, 'Poltek Batam'),
+(4, 'ASDFGHJKL', 'asdfghjkl', 'ASDF', 89876543210, NULL, 'Polibatam'),
+(5, 'Hash', '$2y$12$MjfCUukIPLiUhdrCmxSPu./zw/.ryNGBUy/9Qp37tWqAAsIhcuMGu', 'Hasher', 8765432109, NULL, 'Batam Centre'),
+(6, 'Hash1', '$2y$12$fWr.aEnDhtruOJrTLvX11OYBus4NtuoeigEngkz5gyOAI9CZ673a2', 'Hashing', 81234567899, '', 'Teluk Tering');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjual`
+--
+
+CREATE TABLE `penjual` (
+  `id_penjual` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penjual`
+--
+
+INSERT INTO `penjual` (`id_penjual`, `username`, `password`) VALUES
+(1, 'admin', '$2y$12$M17SFuLLiH1rXasBPuSDvO360L5BVJQIYuVRXb1W.PGXb8ADZ0ZWm');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `kode_pesanan` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL,
+  `konfirmasi_pesanan` enum('ya','tidak') DEFAULT NULL,
+  `tanggal_pesan` date DEFAULT NULL,
+  `kode_mobil` varchar(10) DEFAULT NULL,
+  `metode_pengiriman` enum('delivery','pickup') DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `merek_mobil`
+--
+ALTER TABLE `merek_mobil`
+  ADD PRIMARY KEY (`kode_merek`);
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`kode_mobil`),
+  ADD KEY `id_merek` (`kode_merek`);
+
+--
+-- Indeks untuk tabel `mobil_favorit`
+--
+ALTER TABLE `mobil_favorit`
+  ADD PRIMARY KEY (`id_favorit`),
+  ADD KEY `id_pembeli` (`id_pembeli`);
+
+--
+-- Indeks untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id_pembeli`);
+
+--
+-- Indeks untuk tabel `penjual`
+--
+ALTER TABLE `penjual`
+  ADD PRIMARY KEY (`id_penjual`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`kode_pesanan`),
+  ADD KEY `id_pembeli` (`id_pembeli`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `merek_mobil`
+--
+ALTER TABLE `merek_mobil`
+  MODIFY `kode_merek` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `kode_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id_pembeli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjual`
+--
+ALTER TABLE `penjual`
+  MODIFY `id_penjual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `kode_pesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD CONSTRAINT `mobil_ibfk_1` FOREIGN KEY (`kode_merek`) REFERENCES `merek_mobil` (`kode_merek`);
+
+--
+-- Ketidakleluasaan untuk tabel `mobil_favorit`
+--
+ALTER TABLE `mobil_favorit`
+  ADD CONSTRAINT `mobil_favorit_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`);
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
